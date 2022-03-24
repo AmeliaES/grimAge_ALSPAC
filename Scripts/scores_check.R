9010d994e16ee129dc9aaff73ea55945d2afbd19
AmeliaES <amelia.edmondson-stait@ed.ac.uk>
2022-03-24 16:45:48 +0000

    check warnings from calculator

--- Scripts/scores_check.R
+++ Scripts/scores_check.R
@@ -1,32 +1,41 @@
 # A few error/warnings came up when calculating DNAm ages
 # let's check them here
 
-setwd("/Volumes/cmvm/scs/groups/ALSPAC/data/genomics/B3421/methylation/B3421/grimAge_ALSPAC/")
+setwd("/Volumes/ALSPAC/data/genomics/B3421/methylation/B3421/grimAge_ALSPAC/")
 library(dplyr)
 library(tidyverse)
 library(stringr)
 library(data.table)
+library(ggplot2)
+library(gridExtra)
 # ----------------------------------------------------
-output <- read.csv("Output/scores_15.csv")
+ages <- c("0", "7", "15", "17")
+outputs <- lapply(ages, function(age){
+  x <- read.csv(paste0("Output/scores_", age, ".csv"))
+  x$Female <- as.factor(x$Female)
+  return(x)
+  })
 
 # ----------------------------------------------------
-head(output)
-
-sum(output$Comment == "") # only 85 without a warning
-
-unique(output$Comment)
+# check what warnings we got
+lapply(outputs, function(x) unique(x$Comment))
 
 # " Warning: meanMethBySample is <0.25 MAJOR WARNING:Predicted gender does not agree with column Female."
+# ----------------------------------------------------
+# Plot the meanXchromosome values against Female for all ages
+# Check whether outliers of one sex is near the mean of the other sex
+labels <- paste0("Age ", ages, " years")
+plots <- lapply(1:4, function(i){
+          ggplot(outputs[[i]], aes(x = Female, y = meanXchromosome)) +
+            geom_boxplot() +
+            xlab("Sex") +
+            ggtitle(labels[i])
+          })
+
+do.call("grid.arrange", c(plots, ncol=2))
 
 # ----------------------------------------------------
-# Plot the meanXchromosome values against Female,
-plot(as.factor(output$Female),output$meanXchromosome, ylab = "meanXchromosome" , xlab = "Sex")
-# Outliers of one sex is not near the mean of the other sex, so all looks fine here.
 
-# ----------------------------------------------------
-# How many have the mean < 0.25 warning?
-sum(str_detect(output$Comment, "<0.25")) #103
-nrow(output) #253
 
 
 
